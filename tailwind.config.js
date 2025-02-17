bfb377f0e40eb83b4e14c9ae040c6fc8347a9732
Kiran <kiranmurali93@gmail.com>
2025-02-17 20:22:27 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./popup/**/*.{js,jsx,ts,tsx,html}",
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from "path"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@/popup": path.resolve(__dirname, "./popup"),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: false,
+    rollupOptions: {
+      input: {
+        popup: new URL('./popup/index.html', import.meta.url).pathname,
+        // background: new URL('./background/index.html', import.meta.url).pathname
+      },
+      output: {
+        entryFileNames: "[name]/[name].js"
+      }
+    }
+  }
+})
